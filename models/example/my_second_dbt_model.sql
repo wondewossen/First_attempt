b2eb80103accce2f31efc8628102b1e59437ba7c
wondewossen@gmail.com <wondewossen@gmail.com>
2022-12-28 18:17:53 +0000

    Committed on 12/28/2022

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,38 @@
 
--- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+with customers as (
+    select id as customer_id,
+    first_name,
+    last_name
+    from DEV_WOND_DEMISSIE.TEST.CUSTOMERS
+),
+
+orders as (
+        select id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+        from DEV_WOND_DEMISSIE.TEST.ORDERS
+),
+
+customer_orders as (
+        select customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+        from orders
+    group by 1
+),
+
+final as (
+    select customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders,0) as number_of_orders
+        from customers
+        left join customer_orders using(customer_id)
+
+)
+select * from final
